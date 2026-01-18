3e31de32a66d6e7eed2f3ad3e53e458cd3a9e4b7
AVISHEK DUTTA <109311355+logicbyavishek@users.noreply.github.com>
2026-01-18 21:02:41 +0530

    two way binding done

--- class5/src/App.jsx
+++ class5/src/App.jsx
@@ -1,9 +1,18 @@
 import React, { useState } from 'react'
 
 const App = () => {
+
+  const [alluser, setalluser] = useState(['avi']);
+
   const SubmitHandeler=(e)=>{
     e.preventDefault();
-    console.log(name);
+    setname('');
+
+    const newuser = [...alluser];
+    newuser.push(name);
+    console.log(newuser);
+    setalluser(newuser);
+    
   }
 
   const [name, setname] = useState('');
@@ -15,12 +24,16 @@ const App = () => {
       }}>
         <input 
           type="text" 
-          placeholder='Enter Name' 
+          placeholder='Enter Name'
+          required 
           value={name} 
           onChange={(e)=>setname(e.target.value)}
         />
         <button>Submit</button>
       </form>
+      {alluser.map(function(elem){
+        return <h1>{elem}</h1>
+      })}
     </div>
   )
 }

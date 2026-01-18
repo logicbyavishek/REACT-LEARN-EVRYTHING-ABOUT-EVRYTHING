eeae3414291aeffcadcb3f8adc157380ec9d1e80
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2026-01-18 18:00:34 +0530

    ocChange in form Handeling

--- class5/src/App.jsx
+++ class5/src/App.jsx
@@ -10,7 +10,13 @@ const App = () => {
       <form onSubmit={(e)=>{
         SubmitHandeler(e);
       }}>
-        <input type="text" placeholder='Enter Name' />
+        <input 
+          type="text" 
+          placeholder='Enter Name' 
+          onChange={(e)=>{
+            console.log(e.target.value);
+          }}
+        />
         <button>Submit</button>
       </form>
     </div>
